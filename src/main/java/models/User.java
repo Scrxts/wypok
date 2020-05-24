eb0582b1adb8033ad1b1612c56f541ac3e2764c5
scrxts <djtotyshs@gmail.com>
2020-05-24 15:26:03 +0200

    added User model and 2 servlets

---
+++ src/main/java/models/User.java
@@ -0,0 +1,89 @@
+package models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private Boolean active;
+
+    public User(int id, String username, String password, String email, Boolean active) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.active = active;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                Objects.equals(username, user.username) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(active, user.active);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, email, active);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", active=" + active +
+                '}';
+    }
+}
