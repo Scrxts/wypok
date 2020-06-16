a960f70ddfa7684a6eac570326a632f19b844ee2
Mateusz Ochel <Mateusz.ohel@gmail.com>
2020-06-16 17:49:45 +0200

    Add new class VoteRowMapper in VoteDAOImpl and adding update and read method

--- src/main/java/com/wypok/dao/VoteDAOImpl.java
+++ src/main/java/com/wypok/dao/VoteDAOImpl.java
@@ -1,13 +1,17 @@
 package com.wypok.dao;
 
 import com.wypok.models.Vote;
+import com.wypok.models.VoteType;
 import com.wypok.util.ConnectionProvider;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -47,12 +51,24 @@ public class VoteDAOImpl implements VoteDAO {
 
     @Override
     public Vote read(Long primaryKey) {
-        return null;
+        SqlParameterSource paramSource = new MapSqlParameterSource("vote_id", primaryKey);
+        Vote vote = template.queryForObject(READ_VOTE, paramSource, new VoteRowMapper());
+        return vote;
     }
 
     @Override
-    public boolean update(Vote updateObject) {
-        return false;
+    public boolean update(Vote vote) {
+        boolean result = false;
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("date", vote.getDate());
+        paramMap.put("type", vote.getVoteType().toString());
+        paramMap.put("vote_id", vote.getId());
+        SqlParameterSource paramSource = new MapSqlParameterSource(paramMap);
+        int update = template.update(UPDATE_VOTE, paramSource);
+        if(update > 0) {
+            result = true;
+        }
+        return result;
     }
 
     @Override
@@ -68,5 +84,18 @@ public class VoteDAOImpl implements VoteDAO {
     @Override
     public Vote getVoteByUserIdDiscoveryId(long userId, long discoveryId) {
         return null;
+    }
+}
+
+ class VoteRowMapper implements RowMapper<Vote> {
+    @Override
+    public Vote mapRow(ResultSet resultSet, int row) throws SQLException {
+        Vote vote = new Vote();
+        vote.setId(resultSet.getLong("vote_id"));
+        vote.setUserId(resultSet.getLong("user_id"));
+        vote.setDiscoveryId(resultSet.getLong("discovery_id"));
+        vote.setDate(resultSet.getTimestamp("date"));
+        vote.setVoteType(VoteType.valueOf(resultSet.getString("type")));
+        return vote;
     }
 }
